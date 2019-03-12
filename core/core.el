b7cf0e9dd1cea2e6b791d7fcbf4c875656c880bd
Peter Luladjiev <luladjiev@gmail.com>
2019-03-12 13:04:26 +0200

    Set more variables / Update recentf

--- core/core.el
+++ core/core.el
@@ -20,8 +20,32 @@
 (leet/ensure-folder leet-var-dir)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
-(setq visible-bell t)
-(setq ring-bell-function 'ignore)
+
+(setq visible-bell t
+      ring-bell-function 'ignore
+      ; use version control
+      version-control t
+      ; don't ask for confirmation when opening symlinked file
+      vc-follow-symlinks t
+      ; History & backup settings (save nothing, that's what git is for
+      auto-save-list-file-prefix nil
+      auto-save-default nil
+      create-lockfiles nil
+      history-length 500
+      make-backup-files nil
+      ; Do not show anything at startup
+      inhibit-startup-buffer-menu t
+      inhibit-startup-screen t
+      inhibit-startup-echo-area-message "locutus"
+      initial-buffer-choice t
+      initial-scratch-message ""
+      load-prefer-newer t)
+
+(setq-default indent-tabs-mode nil
+	      tab-width 2
+	      display-line-numbers-type 'relative)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (set-face-attribute 'default nil :font "Hack-16")
 (scroll-bar-mode 0)
@@ -29,15 +53,29 @@
 (menu-bar-mode 0)
 (toggle-frame-maximized)
 (global-display-line-numbers-mode)
-(setq-default display-line-numbers-type 'relative)
-
+(global-auto-revert-mode)
 (global-prettify-symbols-mode)
+
+(defadvice quit-window (before quit-window-always-kill)
+  "When running `quit-window', always kill the buffer."
+  (ad-set-arg 0 t))
+(ad-activate 'quit-window)
+
 (defun indicate-buffer-boundaries-left ()
   (setq indicate-buffer-boundaries 'left))
 (add-hook 'prog-mode-hook #'indicate-buffer-boundaries-left)
 
+; Custom
 (setq custom-file (concat leet-var-dir "custom.el"))
 (when (file-exists-p custom-file)
   (load custom-file t t))
+
+; Recentf
+(add-to-list 'recentf-exclude "^/\\(?:ssh\\|su\\|sudo\\)?:")
+(setq recentf-max-menu-items 0
+      recentf-max-saved-items 300
+      recentf-filename-handlers '(abbreviate-file-name)
+      recentf-save-file (expand-file-name "recentf" leet-var-dir))
+(recentf-mode)
 
 (provide 'core)
